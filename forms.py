288417a6989bfcd003148af6e83e5b781a425d37
Cheick Toure <tourecheick291@gmail.com>
2022-07-15 01:24:14 +0000

    login, search option added

--- forms.py
+++ forms.py
@@ -1,6 +1,12 @@
+from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class HomeForm(FlaskForm):
+    sign_in = SubmitField("Sign In")
+    sign_up = SubmitField("Sign Up")
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,6 +17,11 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Log In')
 
 
 class ResearchForm(FlaskForm):
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect,session
-from forms import RegistrationForm, ResearchForm
+from forms import RegistrationForm, ResearchForm, LoginForm
 from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from util import get_weather,places_api,get_places,filter_categories
 
 
@@ -13,9 +14,28 @@ app.config['SECRET_KEY'] = 'eae4d31ce5ddcbd006c3fca0d8183dd2'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-app.config['SESSION_TYPE'] = 'filesystem'
 db = SQLAlchemy(app)
+crypt = Bcrypt(app)
 
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False) 
+
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+    def check_user_credentials(username,password_candidate):
+        user = User.query.filter_by( username = username).first()
+        if user is None:
+            return False
+        else:
+            password = user.password
+            return crypt.check_password_hash(password,password_candidate)
 
 @app.route("/")
 def home():
@@ -26,13 +46,28 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        password = crypt.generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('search')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+ 
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        if User.check_user_credentials(form.username.data,form.password.data) == False:
+            print("Invalid credential",form.username.data,form.password.data)
+            print(User.check_user_credentials(form.username.data,form.password.data))
+            flash(f'Invalid username or password')
+            return redirect(url_for('login'))
+        else:
+            print("valid credential")
+            return redirect(url_for('search')) # if so - send to home page
+    return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/search", methods=['GET', 'POST'])
